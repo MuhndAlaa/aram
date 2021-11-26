e58049d46839a6c115c47b8d3baa944313dda471
unknown <gehadsamy96@gmail.com>
2021-11-26 04:29:34 +0200

    dashboard update

--- src/scenes/views/DBTasksDisplay/TasksDisplay.jsx
+++ src/scenes/views/DBTasksDisplay/TasksDisplay.jsx
@@ -1,6 +1,38 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+function LinkTab(props) {
+    return (
+      <Tab
+        component="a"
+        onClick={(event) => {
+          event.preventDefault();
+        }}
+        {...props}
+      />
+    );
+  }
 function TasksDisplay({ currentProject, currentBoard }) {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
     return (
         <>
+   <Box  sx={{ width: '100%', margin:'1rem auto', padding:'1rem' }}>
+      <Tabs value={value} onChange={handleChange} aria-label="nav tabs example">
+        <LinkTab label="mind map" href="/drafts" />
+        <LinkTab label="drag and drop" href="/trash" />
+        <Stack spacing={2} direction="row">
+      <Button variant="contained">userS</Button>
+
+    </Stack>
+      </Tabs>
+    </Box>
             <h4 className='tasks-display text-black' >for the views components to be diplayed</h4>
             <h4 className='tasks-display text-black' >Current Project is: {currentProject}</h4>
             <h4 className='tasks-display text-black' >Current Board is: {currentBoard}</h4>
--- src/scenes/views/Dashboard/Dashboard.jsx
+++ src/scenes/views/Dashboard/Dashboard.jsx
@@ -1,57 +1,93 @@
-import Button from "@restart/ui/esm/Button"
-import { useState } from "react"
+import Button from "@restart/ui/esm/Button";
+import { useState } from "react";
 import { Modal } from "react-bootstrap";
-import SideBar from '../SideBar/SideBar';
-import TaskDisplay from '../DBTasksDisplay/TasksDisplay'
-import { Task } from "../Task/Task"
-import './dashboard.scss'
+import SideBar from "../SideBar/SideBar";
+import TaskDisplay from "../DBTasksDisplay/TasksDisplay";
+import { Task } from "../Task/Task";
+import "./dashboard.scss";
 import firebase from "../../../firebase/firebase";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
+import MiniDrawer from "../Sidenavbar/Sidenavbar";
 
 const Dashboard = () => {
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    //Declare current project and current board
-    const [currentProject , setCurrentProject] = useState();
-    const [currentBoard , setCurrentBoard] = useState();
+  //Declare current project and current board
+  const [currentProject, setCurrentProject] = useState();
+  const [currentBoard, setCurrentBoard] = useState();
 
-    //Declare firebase
-    const ref = firebase.firestore()
-    const user = useSelector(state => state.user) //State of user
+  //Declare firebase
+  const ref = firebase.firestore();
+  const user = useSelector((state) => state.user); //State of user
 
-    //Getting assigneed projects from db
-    const assigneeQuery = user?.uid && ref.collection("projects").where("projectAssigneesEmails", "array-contains", user.email)
-    const [assigneeProjects] = useCollectionData(assigneeQuery, { idField: "id" });
+  //Getting assigneed projects from db
+  const assigneeQuery =
+    user?.uid &&
+    ref
+      .collection("projects")
+      .where("projectAssigneesEmails", "array-contains", user.email);
+  const [assigneeProjects] = useCollectionData(assigneeQuery, {
+    idField: "id",
+  });
 
-    //Getting assigneed boards from db
-    const boardsQuery = user?.uid && ref.collectionGroup("boards").where("boardAssigneesEmails", "array-contains", user.email)
-    const [boards] = useCollectionData(boardsQuery, { idField: "id" });
+  //Getting assigneed boards from db
+  const boardsQuery =
+    user?.uid &&
+    ref
+      .collectionGroup("boards")
+      .where("boardAssigneesEmails", "array-contains", user.email);
+  const [boards] = useCollectionData(boardsQuery, { idField: "id" });
 
-    //To update component with live owned projects and assigned projects
-    useEffect(() =>  {
-    }, [user, assigneeProjects , boards ])
-    return (
-        <>
-            <SideBar assigneeProjects={assigneeProjects} boards={boards} setCurrentProject={setCurrentProject} setCurrentBoard={setCurrentBoard} />
-            <TaskDisplay currentProject={currentProject} currentBoard={currentBoard}/>
-            
-            <div>
-                <Button className="addTask text-white" variant="primary" onClick={handleShow}>
-                    +Task
-                </Button>
+  //To update component with live owned projects and assigned projects
+  useEffect(() => {}, [user, assigneeProjects, boards]);
+  return (
+    <div className="d-flex">
+      <MiniDrawer
+        assigneeProjects={assigneeProjects}
+        boards={boards}
+        setCurrentProject={setCurrentProject}
+        setCurrentBoard={setCurrentBoard}
+      />
+      <div>
+        <TaskDisplay
+          currentProject={currentProject}
+          currentBoard={currentBoard}
+        />
 
-                <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false} className="modalTask">
-                    <Modal.Body>   <Task></Task>  </Modal.Body>
-                    <Modal.Footer>
-                        <Button className="closeBtn" onClick={handleClose}>  Close </Button>
-                    </Modal.Footer>
-                </Modal>
-            </div>
-        </>
-    )
-}
+        <div>
+          <Button
+            className="addTask text-white"
+            variant="primary"
+            onClick={handleShow}
+          >
+            +Task
+          </Button>
+
+          <Modal
+            show={show}
+            onHide={handleClose}
+            backdrop="static"
+            keyboard={false}
+            className="modalTask"
+          >
+            <Modal.Body>
+              {" "}
+              <Task></Task>{" "}
+            </Modal.Body>
+            <Modal.Footer>
+              <Button className="closeBtn" onClick={handleClose}>
+                {" "}
+                Close{" "}
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Dashboard;
